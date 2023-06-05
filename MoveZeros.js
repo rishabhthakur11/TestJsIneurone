a737735dac83db9784d8ebd6e9053750f410bc76
Rishabh Thakur <74073486+rishabhthakur11@users.noreply.github.com>
2023-06-05 22:46:04 +0530

    code correction

--- MoveZeros.js
+++ MoveZeros.js
@@ -1,42 +1,24 @@
-function Node(value) {
+function moveZeros(nums) {
+  let i = 0; // Pointer to track non-zero elements
 
-    this.value = value;
-  
-    this.next = null;
-  
-  }
-  
-  
-  function reverseLinkedList(head) {
-  
-    let prev = null;
-  
-    let current = head;
-  
-     while (current !== null) {
-  
-      let next = current.next;
-  
-      current.next = prev;
-  
-      prev = current;
-  
-      current = next;
-  
+  // Iterate over the array and move non-zero elements to the left
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== 0) {
+      nums[i] = nums[j];
+      i++;
     }
-  
-  
-    return prev;
-  
   }
-  
-  
-  var head = new Node(1);
-  
-  head.next = new Node(2);
-  
-  head.next.next = new Node(3);
-  
-  var reversed = reverseLinkedList(head);
-  
-  console.log(reversed.value);
+
+  // Fill the remaining elements with zeros
+  while (i < nums.length) {
+    nums[i] = 0;
+    i++;
+  }
+
+  return nums;
+}
+
+// Example usage
+const nums = [0, 1, 0, 3, 12];
+const result = moveZeros(nums);
+console.log(result); // Output: [1, 3, 12, 0, 0]
